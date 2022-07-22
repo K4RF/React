5b2c7d80259d85c09535df6f51dbc580e1873233
K4RF <rpdirwk1025@naver.com>
2022-07-23 03:17:20 +0900

    Update Detail.js

--- routes/Detail.js
+++ routes/Detail.js
@@ -1,19 +1,42 @@
 import { useEffect, useState } from "react";
+import MovieDetail from "../components/MovieDetail";
 import {useParams} from "react-router-dom";
+import styles from "./Home.module.css";
 
 function Detail(){
     const [loading, setLoading] = useState(true);
+    const [movie, setMovie] = useState(" ");
     const {id} = useParams();
     const getMovie = async() =>{
         const json = await(await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)).json();
         setLoading(false);
-        console.log(json);
+        setMovie(json.data.movie);
     } 
-    useEffect(() => {
+    useEffect(() =>{
         getMovie();
     }, []);
     return (
-        <h1>Detail</h1>
+        <div>
+      {loading ? (
+        <div className={styles.loader}>
+          <span>Loading...</span>
+        </div>
+      ) : (
+        <div>
+          {movie.map((movie) => (
+            <MovieDetail 
+            key={movie.id}
+            year={movie.year}
+            coverImg={movie.large_cover_image}
+            title={movie.title}
+            rating={movie.rating}
+            runtime={movie.runtime}
+            summary={movie.summary}
+            genres={movie.genres} />
+            ))}
+          </div>
+        )}
+      </div>
     );
 }
 
